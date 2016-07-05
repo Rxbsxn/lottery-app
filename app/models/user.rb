c9123d1abad086dd95fe2a1c53b841da9f33bd10
BobrImperator <lordboberro@gmail.com>
2016-07-05 13:34:00 +0200

    Added admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  resourcify
+  rolify :before_add => :before_add_method
+  def before_add_method(role)
+
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
