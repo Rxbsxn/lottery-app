3777ce30d30c5f8d6876b963ac75c6f855eb449a
Robert <harezlak.robert7@gmail.com>
2016-07-19 13:27:21 +0200

    service tdd

---
+++ app/services/draw_service.rb
@@ -0,0 +1,11 @@
+class DrawService
+  def initialize(auction)
+    @auction = auction
+  end
+  def draw
+    
+  end
+
+  private
+  attr_reader :auction, :user, :winner
+end
---
+++ spec/services/draw_service_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+RSpec.describe DrawService, type: :service do
+ pending 'starts draw'
+ pending 'draw with one bidder'
+ pending 'sending email to winner'
+end
